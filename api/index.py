8821865a76ae1f5ec183b328103244ae23af8c58
Deepraj <deepraj21.bera@gmail.com>
2024-03-03 20:10:51 +0530

    Add Flask-SQLAlchemy and User model

--- api/index.py
+++ api/index.py
@@ -1,10 +1,39 @@
 from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
+from flask import Flask, render_template, request, redirect, url_for, session, flash, send_file
 
 app = Flask(__name__)
+
+app.secret_key = 'MYSECRETKEY'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False                
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(120), nullable=False)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
+            session['user_id'] = user.id
+            return redirect(url_for('index'))
+        else:
+            flash('Wrong username or password. Please try again.', 'error')
+
+    return 'success login'
 
 @app.route('/about')
 def about():
