b9890d76b61510e603679127e604ca87b473de6d
Deepraj <deepraj21.bera@gmail.com>
2024-03-04 16:56:52 +0530

    added model for brain tumor

--- app.py
+++ app.py
@@ -2,6 +2,19 @@ from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
 from flask import Flask, render_template, request, redirect, url_for, session, flash, send_file
+from werkzeug.utils import secure_filename
+import os
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
+from tensorflow.keras import Model
+from sklearn.utils import shuffle
+from tqdm import tqdm
+import requests
+import plotly.graph_objects as go
+import urllib.request
 
 app = Flask(__name__)
 
@@ -20,6 +33,51 @@ class User(db.Model):
 def create_tables():
     with app.app_context():
         db.create_all()
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'jpg', 'jpeg', 'png'}
+
+def train_val_generators(TRAINING_DIR, VALIDATION_DIR, TEST_DIR):
+
+    train_datagen = ImageDataGenerator(rescale=1./127.5, rotation_range=30, width_shift_range=0.2,height_shift_range=0.2, shear_range=0.2, zoom_range=0.2,horizontal_flip=True, fill_mode='nearest')
+
+    train_generator = train_datagen.flow_from_directory(directory=TRAINING_DIR, batch_size=32,class_mode='binary', target_size=(150, 150))
+
+    valid_or_test_datagen = ImageDataGenerator(rescale=1./127.5)
+
+    validation_generator = valid_or_test_datagen.flow_from_directory(directory=VALIDATION_DIR, batch_size=32,class_mode='binary', target_size=(150, 150))
+
+    test_generator = valid_or_test_datagen.flow_from_directory(directory=TEST_DIR, batch_size=32,class_mode='binary', target_size=(150, 150))
+    return train_generator, validation_generator, test_generator
+
+base_dir = 'MODELLING/'
+training_dir = os.path.join(base_dir, 'training')
+validation_dir = os.path.join(base_dir, 'validation')
+testing_dir = os.path.join(base_dir, 'testing')
+
+train_generator, validation_generator, test_generator = train_val_generators(training_dir, validation_dir, testing_dir)
+
+
+# Load the trained model
+model = tf.keras.models.load_model('brain_tumor.h5')
+
+def prediction(YOUR_IMAGE_PATH):
+    img = image.load_img(YOUR_IMAGE_PATH, target_size=(150, 150))
+    x = image.img_to_array(img)
+    x /= 127.5
+    x = np.expand_dims(x, axis=0)
+
+    images = np.vstack([x])
+    classes = model.predict(images, batch_size=10)
+    score = tf.nn.sigmoid(classes[0])
+
+    class_name = train_generator.class_indices
+    class_name_inverted = {y: x for x, y in class_name.items()}
+
+    if classes[0] > 0.5:
+        return class_name_inverted[1], 100 * np.max(score)
+    else:
+        return class_name_inverted[0], 100 * np.max(score)
 
 @app.route('/')
 def index():
